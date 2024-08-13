7bd7ea0cffba7e5411f52aac12e46a23d2cb57d5
LanLop1 <lanchazolopezs@gmail.com>
2024-08-13 19:03:19 +0200

    Cmbio dataset trataamiento inicial de datos

--- ia.py
+++ ia.py
@@ -1,12 +1,70 @@
 #%%
+import warnings
+warnings.filterwarnings("ignore")
 import pandas as pd
-import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-import seaborn as sb
+import numpy as np
+import seaborn as sns
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import classification_report
+import colorama
+from colorama import Back
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
 
-datos = pd.read_csv("hearth.csv")
-datos.head()
-datos.describe()
+Main_Dataset=pd.read_csv("heart_2020_cleaned.csv")
+Main_Dataset = pd.DataFrame(Main_Dataset)
+print(Main_Dataset.shape)
+Main_Dataset.head(10)
+def describe(Main_Dataset):
+    
+    
+    variables = []
+    dtypes = []
+    count = []
+    unique = []
+    missing = []
+    min_ = []
+    max_ = []
+    
+    
+    for item in Main_Dataset.columns:
+        variables.append(item)
+        dtypes.append(Main_Dataset[item].dtype)
+        count.append(len(Main_Dataset[item]))
+        unique.append(len(Main_Dataset[item].unique()))
+        missing.append(Main_Dataset[item].isna().sum())
+        
+        if Main_Dataset[item].dtypes == 'float64' or Main_Dataset[item].dtypes == 'int64':
+            min_.append(Main_Dataset[item].min())
+            max_.append(Main_Dataset[item].max())
+        else:
+            min_.append('Str')
+            max_.append('Str')
+        
+
+    output = pd.DataFrame({
+        'variable': variables, 
+        'dtype': dtypes,
+        'count': count,
+        'unique': unique,
+        'missing value': missing,
+        'Min': min_,
+        'Max': max_
+    })    
+        
+    return output
+
+desc_df = describe(Main_Dataset)
+print (desc_df)
 # sb.countplot(x="Survived", data=datos)
 # sb.countplot(x="Survived", data=datos, hue="Sex")
 # datos.isna().sum()
